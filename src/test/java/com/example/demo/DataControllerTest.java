a7717d2b65cdaded7899e1e02bc374f33613e00d
Peter Dada <dapetoo@gmail.com>
2022-09-05 16:15:20 +0100

    Project initialization

---
+++ src/test/java/com/example/demo/DataControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataControllerTest {
+
+    @Autowired
+    DataController dataController;
+
+    @Test
+    void health() {
+        assertEquals( "HEALTH CHECK OK!", dataController.healthCheck());
+    }
+
+    @Test
+    void version() {
+        assertEquals( "The actual version is 1.0.0", dataController.version());
+    }
+
+    @Test
+    void nationsLength() {
+        Integer nationsLength = dataController.getRandomNations().size();
+        assertEquals(10, nationsLength);
+    }
+
+    @Test
+    void currenciesLength() {
+        Integer currenciesLength = dataController.getRandomCurrencies().size();
+        assertEquals(20, currenciesLength);
+    }
+
+}
